e75dd4c7049815dfe6fe746d2a19a8dcb8786124
Nam <namdeveloper.ca@gmail.com>
2022-12-17 20:19:47 +0700

    Down svg from tiktok

---
+++ src/assets/images/index.js
@@ -0,0 +1,7 @@
+const images = {
+  logo: require('~/assets/images/logo.svg').default,
+  loading: require('~/assets/images/loading.svg').default,
+  xmark: require('~/assets/images/xmark.svg').default,
+}
+
+export default images
