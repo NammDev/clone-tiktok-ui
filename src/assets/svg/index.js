afaa6a717592af4b6c14fdb785ab74bbdd3061aa
Nam <namdeveloper.ca@gmail.com>
2022-12-17 21:24:53 +0700

    Style Header > centerContainer

--- src/assets/svg/index.js
+++ src/assets/svg/index.js
@@ -1,3 +1,4 @@
 export { default as Logo } from './logo.svg'
 export { default as Loading } from './loading.svg'
 export { default as Xmark } from './xmark.svg'
+export { default as Search } from './search.svg'
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,7 +1,8 @@
-import styles from './Header.module.scss'
-import classNames from 'classnames/bind'
 import { Link } from 'react-router-dom'
-import { Logo, Loading, Xmark, Button, Upload, Mess, Noti } from '~/assets/svg'
+import classNames from 'classnames/bind'
+import styles from './Header.module.scss'
+import { Logo, Xmark, Search } from '~/assets/svg'
+// Loading, , , Upload, Mess, Noti
 
 const cx = classNames.bind(styles)
 
@@ -9,13 +10,25 @@ function Header() {
   return (
     <div className={cx('header')}>
       <div className={cx('wrapper')}>
-        <Link className={cx('logo')} to='/upload'>
-          <Logo />
-        </Link>
+        <Logo />
 
-        <div className={cx('search')}>
-          <Loading />
-          <Xmark />
+        <div className={cx('centerContainer')}>
+          <div className={cx('formContainer')}>
+            <form className={cx('searchInput')}>
+              <input
+                type='search'
+                placeHolder='Search accounts and videos'
+                className={cx('inputElement')}
+              />
+              <div className={cx('loadingIcon')}>
+                <Xmark />
+              </div>
+              <span className={cx('split')}></span>
+              <button className={cx('buttonSearch')}>
+                <Search />
+              </button>
+            </form>
+          </div>
         </div>
         <div className={cx('actions')}></div>
       </div>
