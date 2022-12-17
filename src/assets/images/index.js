90a13b47c0f171cac9ad043708e98ca4bc5e4d42
Nam <namdeveloper.ca@gmail.com>
2022-12-17 20:46:46 +0700

    Export all svg file to svg/index.js

--- src/assets/images/index.js
+++
@@ -1,7 +0,0 @@
-const images = {
-  logo: require('~/assets/images/logo.svg').default,
-  loading: require('~/assets/images/loading.svg').default,
-  xmark: require('~/assets/images/xmark.svg').default,
-}
-
-export default images
