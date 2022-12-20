890cdad12b766208e4ae9a7f5bf92e38388d8dc6
Nam <namdeveloper.ca@gmail.com>
2022-12-21 03:16:21 +0700

    Config env

--- src/services/searchService.js
+++ src/services/searchService.js
@@ -1,4 +1,4 @@
-import * as request from '~/utils/request.js'
+import * as request from '~/utils/httpRequest.js'
 
 export const search = async (q, type = 'less') => {
   const res = await request.get(`/users/search`, {
